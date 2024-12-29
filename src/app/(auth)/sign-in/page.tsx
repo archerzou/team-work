12e641fac9d7e08506667a05fb2f5310685cca57
Archer Zou <archer.zou84@gmail.com>
2024-12-29 18:38:07 +0800

    response modal

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,14 +1,12 @@
 import { redirect } from "next/navigation";
+
 import { SignInCard } from "@/features/auth/components/sign-in-card";
-import {getCurrent} from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 
 const SignIn = async () => {
     const user = await getCurrent();
     if (user) redirect("/");
 
-    return (
-       <SignInCard />
-    );
+    return <SignInCard />;
 };
-
 export default SignIn;
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
+
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
-import {getCurrent} from "@/features/auth/actions";
+import { getCurrent } from "@/features/auth/queries";
 
 const SignUp = async () => {
     const user = await getCurrent();
